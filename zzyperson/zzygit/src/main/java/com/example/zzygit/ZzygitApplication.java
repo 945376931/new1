f1f3a31dfe44596f7898e1bf9a5c08380a8630c3
945376931 <945376931@qq.com>
2019-08-02 09:56:02 +0800

    zzy1

---
+++ zzyperson/zzygit/src/main/java/com/example/zzygit/ZzygitApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzygitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzygitApplication.class, args);
+    }
+
+}
